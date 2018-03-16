dcb6e59444829c7770e1cdae8c0a8c094f76278b
camander321 <chamburg321@gmail.com>
2018-03-16 14:28:08 -0700

    add and test method to get years on other planets from seconds

--- js/age.js
+++ js/age.js
@@ -1,13 +1,44 @@
 export class Age {
+  constructor() {
+    this.now = new Date("03/16/2018");
+    this.secondsPerYear = 31556925;
+  }
+
   yearsToSeconds(years) {
-    return years * 31556926;
+    return years * this.secondsPerYear;
   }
 
   secondsToYears(seconds) {
-    return seconds / 31556926;
+    return seconds / this.secondsPerYear;
   }
 
   secondsBetweenDates(date1, date2) {
-    return (date2 - date1) / 1000;
+    return date2 / 1000 - date1 / 1000;
+  }
+
+  getYearsOnPlanet(planet, seconds) {
+    let years = this.secondsToYears(seconds);
+    switch (planet) {
+    case 'mercury':
+      return years / 0.24;
+    case 'venus':
+      return years / 0.62;
+    case 'mars':
+      return years / 1.88;
+    case 'jupiter':
+      return years / 11.86;
+    default:
+      return years;
+    }
+  }
+
+  getAgeSeconds(birthday) {
+    return this.secondsBetweenDates(new Date(birthday), this.now);
+  }
+
+  getExpectedRemainingSeconds(birthday, expectancy) {
+    let oldAge = new Date(birthday);
+    oldAge.setYear(oldAge.getFullYear() + expectancy);
+    return this.secondsBetweenDates(this.now, oldAge);
   }
 }
--- spec/age-spec.js
+++ spec/age-spec.js
@@ -10,11 +10,25 @@ describe('Age', function() {
 
 
   it('should take an age in years and convert to seconds', function() {
-    expect(age.yearsToSeconds(24)).toEqual(757366224);
-    expect(age.secondsToYears(757366224)).toEqual(24);
+    expect(age.yearsToSeconds(24)).toEqual(757366200);
+    expect(age.secondsToYears(757366200)).toEqual(24);
   });
 
   it('should take 2 dates and return the difference in seconds', function() {
-    expect(age.secondsBetweenDates(new Date("October 13, 2000"), new Date("October 13, 2024"))).toEqual(757382400);
+    expect(age.secondsBetweenDates(new Date("October 13, 2000"), new Date("October 14, 2000"))).toEqual(86400);
   });
+
+  it('should get current age in seconds', function() {
+    // cant test directly because age is always changing.
+    expect( age.getAgeSeconds("05/05/1994")).toEqual(753062400);
+  });
+
+  it('should get expected remaining seconds in life', function() {
+    expect( age.getExpectedRemainingSeconds("05/05/1994", 87)).toEqual(1992470400);
+  });
+
+  it('should take a date and a planet and return the number', function() {
+    expect(age.getYearsOnPlanet('mars', 753062400)).toEqual(12.6934137715566);
+  });
+
 });
